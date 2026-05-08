f9eaccdb74fef0f5d6a3f5353c18ff1023201e45
DaLinSang <13001898081@163.com>
2026-05-08 08:51:02 +0800

    hot-fix commit2

--- src/main/java/com/itheima/controller/JoinQueryController.java
+++ src/main/java/com/itheima/controller/JoinQueryController.java
@@ -31,6 +31,7 @@ public class JoinQueryController {
         log.info("查询用户订单统计，前{}条", limit);
         log.info("这一条是为了测试Git123456用的");
         log.info("这一条是为了测试Git123456fix用的");
+        log.info("这一条是为了测试hot-fix2用的");
         List<UserOrderStat> list = joinQueryService.findUserOrderStats(limit);
         long end = System.currentTimeMillis();
         log.info("查询耗时：{} 毫秒", end - begin);
